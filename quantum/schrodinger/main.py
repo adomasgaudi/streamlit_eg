386c801a530c011c5d0a9e0162b49c44fe8aa975
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2023-12-24 22:55:22 +0200

    ft: schrodinger streamlit

--- quantum/schrodinger/main.py
+++ quantum/schrodinger/main.py
@@ -1,6 +1,24 @@
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit_aer import AerSimulator
 
 qr_atom = QuantumRegister(1, name="atom")
 qc = QuantumCircuit(qr_atom)
 qc.h(qr_atom)   
+qc.draw('mpl')
+
+backend = AerSimulator()
+
+qr_cat = QuantumRegister(1, name="cat")
+qc = QuantumCircuit(qr_atom, qr_cat)
+qc.h(qr_atom[0])
+qc.cx(qr_atom[0], qr_cat[0])
+qr_observer = QuantumRegister(1, name="observer")
+qc = QuantumCircuit(qr_atom, qr_cat, qr_observer)
+qc.h(qr_atom[0])
+qc.cx(qr_atom[0], qr_cat[0])
+qc.cx(qr_cat[0], qr_observer[0])
+
+qc.measure_all()
+result = backend.run(qc, shots=1000).result()
+print(result.get_counts())
 qc.draw('mpl')
--- quantum/schrodinger/main.stream.py
+++
@@ -1,6 +0,0 @@
-from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
-
-qr_atom = QuantumRegister(1, name="atom")
-qc = QuantumCircuit(qr_atom)
-qc.h(qr_atom)   
-qc.draw('mpl')
